6c1d1116466c15d0671685573aedd0090f38382d
Dylan Porter <dporter042@gmail.com>
2015-01-17 17:18:43 -0600

    Initial Commit

---
+++ DriveHome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DriveHome
+//
+//  Created by Dylan Porter on 1/17/15.
+//  Copyright (c) 2015 Dylan Porter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DriveHome/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DriveHome
+//
+//  Created by Dylan Porter on 1/17/15.
+//  Copyright (c) 2015 Dylan Porter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
