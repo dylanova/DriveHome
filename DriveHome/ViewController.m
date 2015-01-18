807032b70d4104559a10177173ff474ff17cd3cc
Dylan Porter <dporter042@gmail.com>
2015-01-17 18:29:12 -0600

    Moved map to Austin and set the zoom to cover my area; added traffic layer

--- DriveHome/ViewController.m
+++ DriveHome/ViewController.m
@@ -25,19 +25,20 @@
 - (void)viewDidLoad {
     // Create a GMSCameraPosition that tells the map to display the
     // coordinate -33.86,151.20 at zoom level 6.
-    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
-                                                            longitude:151.20
-                                                                 zoom:6];
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:30.2861
+                                                            longitude:-97.7394
+                                                                 zoom:10.9];
     mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
     mapView_.myLocationEnabled = YES;
+    mapView_.trafficEnabled = YES;
     self.view = mapView_;
     
     // Creates a marker in the center of the map.
-    GMSMarker *marker = [[GMSMarker alloc] init];
-    marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
-    marker.title = @"Sydney";
-    marker.snippet = @"Australia";
-    marker.map = mapView_;
+    //GMSMarker *marker = [[GMSMarker alloc] init];
+    //marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
+    //marker.title = @"Sydney";
+    //marker.snippet = @"Australia";
+    //marker.map = mapView_;
 }
 
 - (void)didReceiveMemoryWarning {
